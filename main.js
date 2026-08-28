ca5c208652eeb39ccd3dab3c95bc696ac8800002
Casper Howse <22231@northcote.school.nz>
2026-08-28 15:30:12 +1200

    feat: automatic alerts

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ function sendNotification() {
     notification.onclick = function notificationClick() {
         console.log("notification clicked");
     };
-    
+
     console.log("created notification:", notification);
 }
 
@@ -32,3 +32,55 @@ const sendNotificationButton = document.getElementById(
     "sendNotificationButton",
 );
 sendNotificationButton.onclick = sendNotification;
+
+const alertOffsetMinutes = -1;
+const alertTimesRaw = [
+    "10:00", // P1
+    "11:00", // P2
+    "11:30", // Interval
+    "11:45", // Form
+    "12:45", // P3
+    "13:30", // Lunch
+    "14:30", // P4
+    "15:30", // P5
+];
+
+function translateRawTimes(times) {
+    return times.map((time) => {
+        const split = time.split(":");
+        const date = new Date();
+        date.setHours(Number(split[0]));
+        date.setMinutes(Number(split[1]) + alertOffsetMinutes);
+        return {
+            time: date,
+            activated: false,
+        };
+    });
+}
+
+const alertTimes = translateRawTimes(alertTimesRaw);
+
+console.log(alertTimes);
+
+function checkTime() {
+    const now = new Date();
+    console.log("now:", (now.getTime() / 1000).toFixed(0));
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    for (const alert of alertTimes) {
+        console.log("checking time:", alert);
+        if (
+            alert.time.getHours() === hours &&
+            alert.time.getMinutes() === minutes
+        ) {
+            if (alert.activated) continue;
+            console.log("alerting!!!");
+            sendNotification();
+            alert.activated = true;
+        } else {
+            alert.activated = false;
+        }
+    }
+}
+
+setInterval(checkTime, 1000);
